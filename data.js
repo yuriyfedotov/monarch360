bb56abc3ad94137acbdf8015a14510d4e21d9f22
Y_Fedotov <92554647+yuriyfedotov@users.noreply.github.com>
2023-10-03 11:09:04 +0300

    upload

---
+++ data.js
@@ -0,0 +1,438 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "Холл1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.574598240448152,
+        "pitch": 0.02028628388509901,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0491946040658604,
+          "pitch": 0.01826939083017365,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": -0.4004462679907377,
+          "pitch": 0.019526808170239107,
+          "rotation": 0,
+          "target": "3------"
+        },
+        {
+          "yaw": -1.4721832500257754,
+          "pitch": -0.014895347526650227,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -2.577801839867604,
+          "pitch": 0.03583769301853046,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "Холл2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.9890615799950186,
+        "pitch": 0.02579238820185381,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8614899363281117,
+          "pitch": 0.06131941767893956,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": 2.188652448804617,
+          "pitch": -0.01051181610287344,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": -2.4363193311336158,
+          "pitch": -0.03890056221680105,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": -1.4393505570160734,
+          "pitch": -0.03234977986932108,
+          "rotation": 0,
+          "target": "7--1-2-a-b"
+        },
+        {
+          "yaw": -1.0787749825033917,
+          "pitch": -0.01463874614233518,
+          "rotation": 0,
+          "target": "7--1-2-a-b"
+        },
+        {
+          "yaw": -1.9933135845245467,
+          "pitch": 0.10362380461060106,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6991570295664893,
+          "pitch": -0.18386874711260504,
+          "title": "Дверь будет перекрыта",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "Холл3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.0874855860263022,
+        "pitch": -0.06653024916642813,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.345173262891132,
+          "pitch": -0.017311041900155644,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -1.7043370247610188,
+          "pitch": -0.020234781179674144,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": 0.972736379517892,
+          "pitch": 0.013349785367648082,
+          "rotation": 0,
+          "target": "7--1-2-a-b"
+        },
+        {
+          "yaw": 1.6595554239223258,
+          "pitch": -0.0031677088111017326,
+          "rotation": 0,
+          "target": "7--1-2-a-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3------",
+      "name": "Александровский - левая сторона (С)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.9619363800439764,
+        "pitch": 0.11358478977403763,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8515543880406646,
+          "pitch": 0.04436789101062111,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Бородинский (Баркемп)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.4845300461766335,
+        "pitch": 0.14314737668733635,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9519823730340864,
+          "pitch": 0.01149232555230384,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Георгиевский (С)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.0838413454298319,
+        "pitch": 0.10349871099383989,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7728209223341089,
+          "pitch": -0.03556133120364713,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0953946244514956,
+          "pitch": -0.05725331235621134,
+          "title": "Перегородка",
+          "text": "Будет убрана"
+        }
+      ]
+    },
+    {
+      "id": "6--",
+      "name": "Екатерининский (Е)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.137834559495891,
+        "pitch": -0.013800956336135428,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9652184212258774,
+          "pitch": -0.03104339030524983,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 1.1078557396622557,
+          "pitch": 0.0183796265077234,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--1-2-a-b",
+      "name": "Андреевский 1-2 (A-B)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.7227497926459083,
+        "pitch": -0.13642139549775933,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7835121409496368,
+          "pitch": -0.005923059815811982,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 1.2662657110536841,
+          "pitch": 0.008765522300787865,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.044049814685157,
+          "pitch": 0.008054400794883065,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.9870257658375277,
+          "pitch": 0.007715401745940653,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.92370779621867,
+          "pitch": -0.20332937671766516,
+          "title": "Секция А",
+          "text": ""
+        },
+        {
+          "yaw": 0.8198168267099835,
+          "pitch": -0.16797486787108973,
+          "title": "Секция B",
+          "text": ""
+        },
+        {
+          "yaw": 1.3722030599763642,
+          "pitch": -0.6033365943815419,
+          "title": "Линия перегородки",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
